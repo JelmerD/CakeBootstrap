6360c3e553162cdac903ac9b6380c725549d369d
jelmerd <jelmer.droge@gmail.com>
2022-12-19 14:38:01 +0100

    Updated Helper pages

--- src/View/Helper/HtmlHelper.php
+++ src/View/Helper/HtmlHelper.php
@@ -10,6 +10,59 @@ use Cake\View\Helper;
  */
 class HtmlHelper extends Helper\HtmlHelper
 {
+
+    /**
+     * Holds all the currently opened tags from the open() method
+     *
+     * @var array
+     * @see HtmlHelper::open()
+     */
+    protected $_openTags = [];
+
+    /**
+     * Opens an HTML tag. Use close() to close a tag.
+     *
+     * @param string $name tag to open
+     * @param array<string, mixed> $options Additional HTML attributes of the DIV tag, see above.
+     * @return string
+     * @see HtmlHelper::close()
+     */
+    public function open(string $name, array $options = []): string
+    {
+        # add the tag that is being opened to the list
+        $this->_openTags[] = $name;
+
+        return $this->tag($name, null, $options);
+    }
+
+    /**
+     * Closes an HTML tag.
+     * Will trigger a notice when the provided $name does not match the last opened tag.
+     *
+     * @param string|null $name tag to close, leave blank to close the last opened one
+     * @param bool $check Set to false if you do not want to check the currently opened tags by open(). ie. Can be used to close a div or para without content.
+     * @return string|null
+     * @see HtmlHelper::open()
+     */
+    public function close(string $name = null, bool $check = true): ?string
+    {
+        if ($check || is_null($name)) {
+            $last = array_pop($this->_openTags);
+
+            if (!$last) return null;
+
+            # check if the proved $name matches the $last opened tag
+            if ($name && $last != $name) {
+                trigger_error('HtmlHelper::close() closed a tag that was not opened last. Resulting in a HTML fault.', E_USER_NOTICE);
+            }
+
+            # if no $name is provided, use the last opened tag
+            if (!$name) $name = $last;
+        }
+
+        return $this->tag('/' . $name, null);
+    }
+
     /**
      * @param $title
      * @param $url
